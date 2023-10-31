dd749bd107290b6cf5a2dc5341a252ce07f4b002
aoi1 <tkhsaoi88@gmail.com>
2023-11-01 00:46:58 +0900

    Enable to change message using config file

--- hello-server/main.go
+++ hello-server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
+
+const defaultMessage = "Hello, world! Let's learn Kubernetes!"
 
 func main() {
 	port := os.Getenv("PORT")
@@ -13,13 +16,31 @@ func main() {
 		port = "8080"
 	}
 
+	message, err := readMessageFromConfig()
+	if err != nil {
+		log.Fatalf("Failed to read message from config: %v", err)
+		message = defaultMessage
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world! Let's learn Kubernetes!")
+		fmt.Fprintf(w, message)
 	})
 
 	log.Printf("Starting server on port %s\n", port)
-	err := http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func readMessageFromConfig() (string, error) {
+	configDir := "/etc/config"
+	configPath := fmt.Sprintf("%s/myconfig.txt", configDir)
+
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return defaultMessage, nil
+	}
+
+	return string(data), nil
 }
