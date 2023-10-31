c707555db1ddb2bba5fe3ac291db66d05cd234c6
aoi1 <tkhsaoi88@gmail.com>
2023-11-01 00:24:41 +0900

    Enable to select port number by env variable

--- hello-server/main.go
+++ hello-server/main.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, world! Let's learn Kubernetes!")
 	})
 
-	log.Println("Starting server on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	log.Printf("Starting server on port %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
